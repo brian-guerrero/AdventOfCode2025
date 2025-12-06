aeced80782b3de0b9a6475a158e94c7c29d27667
Brian Guerrero <13bguerrero@gmail.com>
2025-12-05 20:41:32 -0600

    Day 5 Part 2

--- Day5.cs
+++ Day5.cs
@@ -12,7 +12,8 @@ foreach (var ingredient in ingredients)
 }
 
 Console.WriteLine($"There are {freshAvalableCount} fresh ingredients available.");
-
+var freshItems = ranges.GetCleanedRanges().Sum(r => r.MaxFreshId - r.MinFreshId + 1);
+Console.WriteLine($"There are {freshItems} fresh ingredients in total.");
 public static class IngredientParser
 {
     public static (FreshIngredientRange[] Ranges, AvailableIngredient[] AvailableIngredients) Parse(string path)
@@ -51,6 +52,29 @@ public static class IngredientExtensions
         public bool HasIngredient(AvailableIngredient ingredient)
         {
             return ranges.Any(r => ingredient.IngredientId >= r.MinFreshId && ingredient.IngredientId <= r.MaxFreshId);
+        }
+
+        public IEnumerable<FreshIngredientRange> GetCleanedRanges()
+        {
+            var sortedRanges = ranges.OrderBy(r => r.MinFreshId).ToList();
+            var cleanedRanges = new List<FreshIngredientRange>();
+
+            foreach (var range in sortedRanges)
+            {
+                if (cleanedRanges.Count == 0 || range.MinFreshId > cleanedRanges.Last().MaxFreshId)
+                {
+                    cleanedRanges.Add(range);
+                }
+                else
+                {
+                    var lastRange = cleanedRanges.Last();
+                    cleanedRanges[cleanedRanges.Count - 1] = new FreshIngredientRange(
+                        lastRange.MinFreshId,
+                        Math.Max(lastRange.MaxFreshId, range.MaxFreshId));
+                }
+            }
+
+            return cleanedRanges;
         }
     }
 }
